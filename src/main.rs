4cd843cc49343e4dffe7e6dbca2416136692e1d2
DestinyofYeet <57575330+DestinyofYeet@users.noreply.github.com>
2024-08-06 17:10:58 +0200

    added example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, 
+    event,
+};
+
+fn main() -> std::io::Result<()> {
+    
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Hello World"),
+        ResetColor
+    )?;    
+    Ok(())
 }
